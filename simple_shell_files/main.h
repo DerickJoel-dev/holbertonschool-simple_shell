08fcdebca022348e593b89d0964563344a61bceb
DerickJoel-dev <derickdev2@gmail.com>
2025-01-04 16:20:56 -0400

    Add main.h

---
+++ simple_shell_files/main.h
@@ -0,0 +1,25 @@
+#ifndef simpleshell
+#define simpleshell
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <signal.h>
+
+extern char **environ;
+
+int main(void);
+int execute(char *line_input);
+
+char **tokenize(char *line);
+char **tokenize_path(char *p);
+char *get_path(char *cmd);
+
+void print_env(void);
+void free_tokens(char **tokens);
+void signal_handler(int signal);
+
+#endif
