47322bd479498abd41a92931a0b6ebc6349fd662
DerickJoel-dev <derickdev2@gmail.com>
2025-01-04 16:12:58 -0400

    Update simple shell files

---
+++ simple_shell_files/execute.c
@@ -0,0 +1,48 @@
+ #include "main.h"
+/**
+ * execute - Executes a command
+ * @line_input: Line input from the user
+ *
+ * Return: exit_stat (0 on success or -1 on error)
+ */
+int execute(char *line_input)
+{
+	char **args = NULL;
+	char *path = NULL;
+	pid_t pid;
+	int status, exit_stat = 0;
+
+	args = tokenize(line_input);
+	if (args == NULL)
+		return (-1); /** Return -1 if tokenization fails */
+
+	if (line_input[0] == '/')
+		path = strdup(line_input); /** use the input as path if it starts with '/' */
+	else
+		path = get_path(args[0]);
+	if (path == NULL) /** Failed to retrieve path */
+	{
+		free_tokens(args);
+		return (-1);
+	}
+	pid = fork(); /** Create a child process */
+	if (pid < 0) /** fork failure */
+	{
+		free_tokens(args);
+		free(path);
+		return (-1);
+	}
+	else if (pid == 0) /** child process */
+	{
+		exit_stat = execve(path, args, environ);
+	}
+	else /** Parent process */
+	{
+		wait(&status);
+		if (WIFEXITED(status))
+			status = WEXITSTATUS(status); /** Get child process exit status */
+	}
+	free_tokens(args);
+	free(path);
+	return (exit_stat);
+}
---
+++ simple_shell_files/main.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (On success) or -1 (on error)
+ */
+
+int main(void)
+{
+	char *line_input = NULL;
+	int ex = 0, r;
+	size_t bufsize = 0;
+
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			printf("simpleshell$ ");
+
+		signal(SIGINT, signal_handler);
+
+		r = getline(&line_input, &bufsize, stdin);
+		if (r == -1)
+		{
+			if (feof(stdin))
+				printf("\n");
+			break;
+		}
+
+		if (strcmp(line_input, "exit\n") == 0)
+			break;
+
+		if (strcmp(line_input, "env\n") == 0)
+		{
+			print_env();
+			continue;
+		}
+
+		ex = execute(line_input);
+		if (ex == -1)
+			perror("Execution error");
+	}
+
+	free(line_input);
+	return (ex);
+}
