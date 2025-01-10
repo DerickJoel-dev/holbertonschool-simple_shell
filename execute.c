2aa46dc9236c76a76ebab78d7d42b507e900d30b
DerickJoel-dev <derickdev2@gmail.com>
2025-01-09 21:33:35 -0400

    Add simple shell files

--- execute.c
+++ execute.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <sys/stat.h>
 
 /**
  * execute - Executes a command
@@ -9,19 +10,27 @@
 int execute(char *command)
 {
     char *args[2];
+    struct stat st;
     pid_t pid;
     int status;
 
-    args[0] = command; /* Pass the command */
-    args[1] = NULL;    /* Null-terminate the arguments array */
+    args[0] = command;
+    args[1] = NULL;
 
-    pid = fork(); /* Fork a child process */
+    /* Cambio: Verificación de existencia y permisos */
+    if (stat(command, &st) == -1 || !(st.st_mode & S_IXUSR))
+    {
+        fprintf(stderr, "%s: No such file or directory\n", command);
+        return (-1);
+    }
+
+    pid = fork();
     if (pid < 0)
     {
         perror("fork error");
         return (-1);
     }
-    else if (pid == 0) /* Child process */
+    else if (pid == 0)
     {
         if (execve(args[0], args, environ) == -1)
         {
@@ -29,9 +38,9 @@ int execute(char *command)
             exit(EXIT_FAILURE);
         }
     }
-    else /* Parent process */
+    else
     {
-        wait(&status); /* Wait for child to finish */
+        wait(&status);
     }
 
     return (0);
